1265379acb229c2fb8d4a79f05bde00d74af4ede
Franc1sco <franug13@gmail.com>
2022-06-06 19:16:08 +0200

    Revert "Cambiar password acceso"
    This reverts commit 9a9089c9aaf11024e06f29c5e6088c12b9b0ab86.

--- app/src/main/java/com/example/audiovisualmanager/DatabaseHelper.kt
+++ app/src/main/java/com/example/audiovisualmanager/DatabaseHelper.kt
@@ -2,6 +2,6 @@ package com.example.audiovisualmanager
 
 class DatabaseHelper {
     fun isValidUser(username: String, password: String): Boolean {
-        return username == "jonay" && password == "admin"
+        return username == "admin" && password == "admin"
     }
 }
