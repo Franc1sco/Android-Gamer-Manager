9a9089c9aaf11024e06f29c5e6088c12b9b0ab86
Franc1sco <franug13@gmail.com>
2022-06-06 19:12:49 +0200

    Cambiar password acceso

--- app/src/main/java/com/example/audiovisualmanager/DatabaseHelper.kt
+++ app/src/main/java/com/example/audiovisualmanager/DatabaseHelper.kt
@@ -2,6 +2,6 @@ package com.example.audiovisualmanager
 
 class DatabaseHelper {
     fun isValidUser(username: String, password: String): Boolean {
-        return username == "admin" && password == "admin"
+        return username == "jonay" && password == "admin"
     }
 }
